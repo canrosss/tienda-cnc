20b8327b8facacacf49c22edd63557172b1f0f55
Canros <canros.mx@gmail.com>
2018-02-06 01:07:28 -0600

    Se añaden icon a categorias, pero falta ajustarlo mejor

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180203080602) do
+ActiveRecord::Schema.define(version: 20180206062818) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "icon"
   end
 
   create_table "products", force: :cascade do |t|
