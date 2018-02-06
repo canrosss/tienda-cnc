19dfe90c6dc7abfba003a0f4db2a703dec572120
Canros <canros.mx@gmail.com>
2018-02-05 21:07:35 -0600

    Se añaden pruebas y validaciones para modelo category

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,4 +4,33 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @category = Category.new(name: "bedmill")
+  end
+
+  test "category should be valid" do
+    assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = "  "
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    category2 = Category.new(name: "bedmill")
+    assert_not category2.valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "aa"
+    assert_not @category.valid?
+  end
+
 end
